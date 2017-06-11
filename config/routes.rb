9f3803c5db517a27e98f65c0aa83f6d6d5a74693
Bill Fennell <bill.fennell@gmail.com>
2017-06-11 13:00:21 +0200

    Add orders controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
+	root 'static_pages#landing_page'
+
   resources :products
+
+  resources :orders, only: [:index, :show, :create, :destroy]
 
   get 'static_pages/about'
 
@@ -8,5 +12,5 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
