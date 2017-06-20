d7d472112d9b2ae295b147fbd171799341267541
Bill Fennell <bill.fennell@gmail.com>
2017-06-20 13:39:15 +0200

    Add authorization checking

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show, :index]
   load_and_authorize_resource
 
   # ADDING AUTHENTICATION
@@ -80,6 +81,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name)
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
     end
 end
