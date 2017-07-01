b127e2b4609b0040f14da5d6b0270d44b9e50dbe
Bill Fennell <bill.fennell@gmail.com>
2017-07-01 03:06:34 +0200

    5.11 Clean up unnecessary comments

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -19,19 +19,8 @@ class MessagesController < ApplicationController
 	   	UserMailer.contact_form(@email, @name, @content).deliver_now
       render :action => 'thank_you'
     else
-		  # @all = @name + ' ' + @email + ' ' + @content
-    	# flash[:notice] = "#{@all}"
-    	# flash[:notice] = "Name: #{@message.name}"
-    	# redirect_to root_url
       render :action => 'new'
     end
   end
-
-	# def thank_you
-	  # @name = params[:name]
-	  # @email = params[:email]
-	  # @message = params[:content]
-   #   	UserMailer.contact_form(@email, @name, @content).deliver_now
-	# end	
 
 end	
