16af211f4400a2931ea0494602e247636a0cf61e
Bill Fennell <bill.fennell@gmail.com>
2017-07-10 02:20:50 +0200

    6.12 Update redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,10 @@
 # config/initlializers/redis.rb
 
+# $redis = Redis::Namespace.new("my_app", :redis => Redis.new)
+
 if Rails.env.production?
-	$redis = Redis.new(url: ENV["REDIS_URL"])
+	# $redis = Redis.new(url: ENV["REDIS_URL"])
+	$redis = Redis.new
 else
 	$redis = Redis.new
 	# $redis = Redis.new(:host => 'localhost', :port => 6379)
